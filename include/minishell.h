de2b539b5b39edca8270868f3a2cebc3168ce611
罗杰 <rmarrero@cbr10s5.42barcelona.com>
2025-04-01 13:07:03 +0200

    exit.c [OK], nominette [OK]

--- include/minishell.h
+++ include/minishell.h
@@ -25,40 +25,14 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
-// -- lexer.h -- //
-typedef enum
-{
-	TOKEN_COMMAND,
-	TOKEN_OPERATOR,
-	TOKEN_SEPARATOR,
-	TOKEN_REDIRECTION,
-	TOKEN_ARGUMENT,
-	TOKEN_END
-}						TokenType;
-
-typedef struct s_token
-{
-	char				*value;
-	TokenType			type;
-	struct s_token		*next;
-}						t_token;
-
-t_token					*lexer(char *str);
-t_token					*init_lexer(char *token);
-t_token					*get_last_token(t_token *token);
-void					add_back(t_token **tokens, t_token *token);
-
 // -- minishell.h -- //
 
 // -- internal strcts -- //
-typedef struct s_token	t_token;
 
-void					print_tokens(t_token *tokens);
-char					*token_type_to_string(TokenType type);
-void					ft_exit(int status, char *msg);
-void					main_loop(char *delimiter, int *p_fd);
-void					ft_here_doc(char *delimiter);
+void	main_loop(char *delimiter, int *p_fd);
+void	ft_here_doc(char *delimiter);
 
-// -- free -- //
-void					free_split(char **split);
+// -- exit.h -- //
+void	ft_exit(int status, char *msg);
+
 #endif
---
+++ src/exit.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 13:05:47 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/04/01 13:05:50 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+void	ft_exit(int status, char *msg)
+{
+	ft_putstr_fd("minishell: ", STDERR_FILENO);
+	ft_putendl_fd(msg, STDERR_FILENO);
+	exit(status);
+}
