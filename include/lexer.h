d44d4b6d3b96487190d059afb15fbbde382ab8f3
罗杰 <rmarrero@cbr10s5.42barcelona.com>
2025-04-01 13:36:34 +0200

    lexer.h [OK]

---
+++ include/lexer.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 11:56:13 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/03/27 11:57:56 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LEXER_H
+# define LEXER_H
+
+// -- lexer.h -- //
+typedef enum
+{
+	TOKEN_COMMAND,
+	TOKEN_OPERATOR,
+	TOKEN_SEPARATOR,
+	TOKEN_REDIRECTION,
+	TOKEN_ARGUMENT,
+	TOKEN_END
+}					TokenType;
+
+typedef struct s_token
+{
+	char			*value;
+	TokenType		type;
+	struct s_token	*next;
+}					t_token;
+
+t_token				*lexer(char *str);
+t_token				*init_lexer(char *token);
+t_token				*get_last_token(t_token *token);
+void				add_back(t_token **tokens, t_token *token);
+void				print_tokens(t_token *tokens);
+char				*token_type_to_string(TokenType type);
+
+#endif
