98953b156ebc022cacb47570752463c5313c5a36
罗杰 <rogerdevworld@gmail.com>
2025-04-02 18:09:21 +0200

    free_split [OK], norminette [OK], add new file for functions

---
+++ src/lexer/free_split.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_split.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 17:36:52 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/03/27 17:36:53 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../include/minishell.h"
+
+// -- free split general -- //
+void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	if (!split)
+		return ;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
