5f18533a20f840ba9589b5ea3048e2e8c97c8846
罗杰 <rogerdevworld@gmail.com>
2025-04-03 01:17:15 +0200

    new file executor

---
+++ include/executor.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 11:56:13 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/03/27 11:57:56 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef EXECUTOR_H
+# define EXECUTOR_H
+
+void    ft_check_executor(t_command *cmd, t_token *token, char **envp);
+void	ft_exec_cmd(t_command *cmd, char **envp);
+void	ft_pipe_and_fork(t_command *cmd, char **env);
+
+#endif
