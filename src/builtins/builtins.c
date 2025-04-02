c6b8771e8a1e8cea3476acc2eba90089ecfd35f2
罗杰 <rogerdevworld@gmail.com>
2025-04-02 14:31:33 +0200

    pwd.c [OK], norminette chage

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -25,27 +25,28 @@ builtin_cmd	get_builtin_cmd(char *cmd)
 		return (EXPORT);
 	if (strcmp(cmd, "unset") == 0)
 		return (UNSET);
-    if (strcmp(cmd, "clear") == 0)
+	if (strcmp(cmd, "clear") == 0)
 		return (CLEAR);
 	return (-1);
 }
 
-void execute_builtin(builtin_cmd cmd, t_token *know_token)
+void	execute_builtin(builtin_cmd cmd, t_token *know_token)
 {
-    if (cmd == CD)
-        ft_printf("Ejecutando cd\n");
-    else if (cmd == EXIT)
-        ft_printf("Ejecutando exit\n");
-    else if (cmd == ECHO)
-        ft_printf("Ejecutando echo\n");
-    else if (cmd == PWD)
-        pwd();
-    else if (cmd == EXPORT)
-        ft_printf("Ejecutando export\n");
-    else if (cmd == UNSET)
-        ft_printf("Ejecutando unset\n");
-    else if (cmd == CLEAR)
-        clear();
-    else
-        ft_printf("Type -> %s Know Token - > %s\n", token_type_to_string(know_token->type), know_token->value);
+	if (cmd == CD)
+		ft_printf("Ejecutando cd\n");
+	else if (cmd == EXIT)
+		ft_printf("Ejecutando exit\n");
+	else if (cmd == ECHO)
+		ft_printf("Ejecutando echo\n");
+	else if (cmd == PWD)
+		pwd();
+	else if (cmd == EXPORT)
+		ft_printf("Ejecutando export\n");
+	else if (cmd == UNSET)
+		ft_printf("Ejecutando unset\n");
+	else if (cmd == CLEAR)
+		clear();
+	else
+		ft_printf("Type -> %s Know Token - > %s\n",
+			token_type_to_string(know_token->type), know_token->value);
 }
