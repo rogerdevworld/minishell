26447304a265e2d4c9f93c57a3f8598acaaa4580
罗杰 <rogerdevworld@gmail.com>
2025-04-02 14:13:23 +0200

    pwd.c [OK], add new function of builtins pwd(local)

---
+++ src/builtins/pwd.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 13:05:47 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/04/01 13:05:50 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../include/minishell.h"
+
+// -- char *getcwd(char *buf, size_t size); -- //
+void	pwd(void)
+{
+	char cwd[1024];
+
+	if (getcwd(cwd, sizeof(cwd)))
+		ft_printf("%s\n", cwd);
+	else
+		perror("getcwd() error");
+}
