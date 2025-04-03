7bf4c6598e2bb91c9e62dd86644c5cf466b342db
罗杰 - Roger <rmarrero@cbr11s6.42barcelona.com>
2025-04-03 15:37:11 +0200

    comment funtions for leaks

--- src/minishell.c
+++ src/minishell.c
@@ -16,16 +16,17 @@
 void	main_loop(char **envp)
 {
 	char		*line;
-	t_token		*tokens;
-	t_command	*cmd;
+	//t_token		*tokens;
+	//t_command	*cmd;
 
-	tokens = NULL;
+	//tokens = NULL;
+	envp =envp;
 	while (1)
 	{
 		line = readline(meta_path(envp));
 		if (!line)
 			break ;
-		tokens = lexer(line);
+		//tokens = lexer(line);
 		if (*line)
 			add_history(line);
 		if (ft_strncmp(line, "exit", 4) == 0)
@@ -33,8 +34,8 @@ void	main_loop(char **envp)
 			free(line);
 			break ;
 		}
-		cmd = parse_tokens(tokens, envp);
-		ft_check_executor(cmd, tokens, envp);
+		//cmd = parse_tokens(tokens, envp);
+		//ft_check_executor(cmd, tokens, envp);
 		free(line);
 	}
 	exit(0);
@@ -54,3 +55,6 @@ ls -la > text && cat -e text && echo "holo world" > new_text && cat text
 && echo hola && cat < new_text || ls -la > text && wc - l text > text_c
 && cat text*
 */
+
+//valgrind --leak-check=full --show-leak-kinds=all ./minishell
+//valgrind --leak-check=full ./minishell
