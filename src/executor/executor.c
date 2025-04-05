09010843cee670427261dca12cc9f856ceec4a33
罗杰 <rogerdevworld@gmail.com>
2025-04-05 04:40:58 +0200

    new functions for | estable  only pipes example ls -la | cat -e | wc -l | cat -e result number$

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -9,25 +9,31 @@
 /*   Updated: 2025/04/01 13:05:50 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../include/minishell.h"
+#include "../../include/minishell.h"
 
 void	ft_check_executor(t_command *cmd, char **envp)
 {
-	int	i;
+	int		p_fd[2];
+	int		prev_fd;
+	pid_t	pid;
+	int		builtin_id;
 
-	if (!cmd->args[0])
-		return ;
-	envp = envp;
+	prev_fd = -1;
 	while (cmd)
 	{
-		i = 0;
-		if (get_builtin_cmd(cmd->args[i]) != -1)
- 			execute_builtin(get_builtin_cmd(cmd->args[i]), cmd->args[i + 1], envp);
-		else
+		if (!cmd->args || !cmd->args[0])
 		{
- 			ft_exec_cmd(cmd, envp);
+			cmd = cmd->next;
+			continue ;
 		}
- 		i++;
+		builtin_id = get_builtin_cmd(cmd->args[0]);
+		if (builtin_id != -1 && cmd->operator!= PIPE)
+			execute_builtin(builtin_id, cmd->args[1], envp);
+		else
+			pid = external_command(cmd, envp, builtin_id, &prev_fd,
+					p_fd);
+		if (cmd->operator!= PIPE)
+			waitpid(pid, NULL, 0);
 		cmd = cmd->next;
 	}
 }
@@ -39,7 +45,7 @@ void	ft_exec_cmd(t_command *cmd, char **envp)
 
 	pid = fork();
 	if (pid == -1)
-		ft_exit("filed fork");
+		ft_exit("Failed to fork");
 	if (pid == 0)
 	{
 		if (execve(get_path(cmd->args[0], envp), cmd->args, envp) == -1)
@@ -48,4 +54,60 @@ void	ft_exec_cmd(t_command *cmd, char **envp)
 	}
 	else if (pid > 0)
 		waitpid(pid, NULL, 0);
+}
+
+pid_t	external_command(t_command *cmd, char **envp, int builtin_id,
+		int *prev_fd, int p_fd[2])
+{
+	pid_t	pid;
+
+	if (cmd->operator== PIPE)
+	{
+		if (pipe(p_fd) == -1)
+			ft_exit("pipe failed");
+	}
+	pid = fork();
+	if (pid == -1)
+		ft_exit("fork failed");
+	if (pid == 0)
+		child_process(cmd, envp, builtin_id, *prev_fd, p_fd);
+	else
+		parent_process(cmd, prev_fd, p_fd);
+	return (pid);
+}
+
+void	child_process(t_command *cmd, char **envp, int builtin_id, int prev_fd,
+		int p_fd[2])
+{
+	if (prev_fd != -1)
+	{
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (cmd->operator== PIPE)
+	{
+		close(p_fd[0]);
+		dup2(p_fd[1], STDOUT_FILENO);
+		close(p_fd[1]);
+	}
+	if (builtin_id != -1)
+		execute_builtin(builtin_id, cmd->args[1], envp);
+	else
+		execve(get_path(cmd->args[0], envp), cmd->args, envp);
+	ft_exit("command failed");
+}
+
+void	parent_process(t_command *cmd, int *prev_fd, int p_fd[2])
+{
+	if (*prev_fd != -1)
+		close(*prev_fd);
+	if (cmd->operator== PIPE)
+	{
+		close(p_fd[1]);
+		*prev_fd = p_fd[0];
+	}
+	else
+	{
+		*prev_fd = -1;
+	}
 }
