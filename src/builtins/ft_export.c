490508265cca45c628a30f0872eb2ec1c01a6f8a
罗杰 <rogerdevworld@gmail.com>
2025-04-09 07:13:11 +0200

    mensaje en export y add env

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -11,6 +11,12 @@
 /* ************************************************************************** */
 #include "../include/minishell.h"
 
+// -- ya esta funciona el env para aplciarle al unset y el export 
+// nuestra propia versiond el env y la copia hay funcion para agregar un elemento
+// voy a cear una para expandir las key a su valor algo como:
+// echo $PATH salida my_getenv("PATH") y neustra lista deberia contener adicionales 
+// export a=a seria como add a=a  en t_env con la funcion ft_env_add(key, value);
+// al final de la lista de t_env
 /*void	ft_export(char **args, char **envp)
 {
 	if (!args[1])
--- src/env.c
+++ src/env.c
@@ -66,6 +66,26 @@ void	ft_env(t_env **env_list, char **envp)
 	}
 }
 
+// Agrega un nuevo par key=value a la lista de entorno
+void ft_env_add(t_env **env_list, const char *key, const char *value)
+{
+    t_env *new_node;
+    
+    new_node = ft_env_new(key, value);
+    if (!new_node)
+        return ;
+    if (!*env_list)
+    {
+        *env_list = new_node;
+        return ;
+    }
+    t_env *tmp = *env_list;
+    while (tmp->next)
+        tmp = tmp->next;
+    tmp->next = new_node;
+}
+
+
 void	print_env(t_myenv *myenv)
 {
 	t_env	*tmp;
