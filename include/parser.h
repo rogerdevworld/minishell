97cf5845f23c22f2dc34f464aad0e77727d7baf3
Xenia Viladrich Valarezo <xviladri@student.42barcelona.com>
2025-06-29 14:46:45 +0200

    norminette fixed of the parser

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: xviladri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 11:56:13 by rmarrero          #+#    #+#             */
-/*   Updated: 2025/03/27 11:57:56 by rmarrero         ###   ########.fr       */
+/*   Updated: 2025/06/28 18:36:00 by xviladri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PARSER_H
@@ -33,22 +33,17 @@ typedef struct s_command
 	t_operator			operator;
 	struct s_command	*next;
 }						t_command;
-
-// guardamos comandos (["echo", "hola"])
-// path del comando
-// archivo entrada por si hay "<"
-// archivo salida por si hay ">"
-// 1 si es ">>", 0 si es ">"
-// para encadenar comandos cuando hay un pipe "|| &&
-//" falta ver si es un file el siguente comando ejemplo "cat -e file"
-
+// -- parser.c -- //
 t_command				*init_command(void);
 void					handle_redirect(t_command *cmd, t_token **tokens);
-char					*remove_quotes(char *str);
+static int				check_syntax(t_token *tokens, t_token *prev);
+void					process_token(t_token **tokens, t_command *current, int *i, char **envp);
 t_command				*parse_tokens(t_token *tokens, char **envp);
-void					print_command_list(t_command *cmds);
-t_operator				resolve_operator(char *operator);
+// -- get_path.c -- //
+char					*remove_quotes(char *str);
 char					*get_path(char *cmd, char **env);
+// -- ft_open.c -- //
+int						ft_open(char *file, int mode);
 const char				*operator_to_str(t_operator op);
-
+t_operator				resolve_operator(char *operator);
 #endif
