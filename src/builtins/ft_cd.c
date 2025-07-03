e4aee45a1c8f07025553aefb9760b3daf733dea0
罗杰 - Roger <rmarrero@cbr6s6.42barcelona.com>
2025-07-03 15:02:44 +0200

    last version with status

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 13:05:47 by rmarrero          #+#    #+#             */
-/*   Updated: 2025/07/03 14:39:16 by rmarrero         ###   ########.fr       */
+/*   Updated: 2025/07/03 14:59:56 by rmarrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/minishell.h"
@@ -16,7 +16,11 @@ int	msg(char *error, char *arg)
 {
 	ft_putstr_fd(error, 2);
 	if (arg)
+	{
+		ft_putstr_fd(" ", 2);
 		ft_putstr_fd(error, 2);
+	}
+	ft_putstr_fd("\n", 2);
 	return (1);
 }
 
@@ -27,7 +31,7 @@ int	ft_cd(char *path, char **envp)
 	if (!path)
 	{
 		//chdir(ft_getenv("HOME", envp));
-		return (1);
+		return (msg("cd: error retrieving current directory: getcwd: cannot access parent directories: No such file or directory", NULL));
 	}
 	if (chdir(path) == -1)
 	{
