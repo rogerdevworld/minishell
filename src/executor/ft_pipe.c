c0b8a0a89ed33a9257c85d519c10dda092624fdd
罗杰 - Roger <rmarrero@cbr6s3.42barcelona.com>
2025-07-16 20:36:21 +0200

    add new file with ft_pipes  ok

---
+++ src/executor/ft_pipe.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_pipe.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 20:16:29 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/07/16 20:16:34 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../include/minishell.h"
+
+int	execute_pipe(t_ast *node, char **envp, t_myenv *myenv,
+		t_minishell *minishell)
+{
+	int		fds[2];
+	int		status;
+	pid_t	pid1;
+	pid_t	pid2;
+
+	if (pipe(fds) == -1)
+		return (1);
+	g_signal = S_CMD;
+	pid1 = fork();
+	status = 0;
+	if (pid1 == 0)
+	{
+		set_defaul_signals();
+		close(fds[0]);
+		dup2(fds[1], STDOUT_FILENO);
+		exit(execute_ast(node->left, envp, myenv, minishell, status));
+	}
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		set_defaul_signals();
+		close(fds[1]);
+		dup2(fds[0], STDIN_FILENO);
+		exit(execute_ast(node->right, envp, myenv, minishell, status));
+	}
+	close(fds[0]);
+	close(fds[1]);
+	waitpid(pid1, &status, 0);
+	update_exit_status(status, minishell);
+	waitpid(pid2, &status, 0);
+	status = update_exit_status(status, minishell);
+	return (status);
+}
