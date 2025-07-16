bf4155e5288c289c27dfc0f9c9df8d438ced3718
罗杰 - Roger <rmarrero@cbr6s3.42barcelona.com>
2025-07-16 21:17:51 +0200

    update with parser && and ||

---
+++ src/syntax/ft_syntax_logic.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_syntax_logic.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmarrero <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/16 20:58:00 by rmarrero          #+#    #+#             */
+/*   Updated: 2025/07/16 20:58:07 by rmarrero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../../include/minishell.h"
+
+// && y ||
+t_ast	*parse_expression(t_token **tokens, char **envp)
+{
+	t_ast *left;
+	t_ast *new;
+	t_node_type type;
+
+	left = parse_pipeline(tokens, envp);
+	while (*tokens && ((*tokens)->type == TOKEN_AND
+			|| (*tokens)->type == TOKEN_OR))
+	{
+		if ((*tokens)->type == TOKEN_AND)
+			type = NODE_AND;
+		else
+			type = NODE_OR;
+		next_token(tokens);
+		new = init_ast_node(type, NULL);
+		new->left = left;
+		new->right = parse_pipeline(tokens, envp);
+		left = new;
+	}
+	return (left);
+}
